6ae6c2334cc065cf67464fd13bd83b05f8ac8558
tiannian <dtiannian@gmail.com>
2024-02-04 17:54:06 +0800

    complete security

--- proto/src/security.rs
+++ proto/src/security.rs
@@ -5,41 +5,31 @@ pub struct SecurityFrameParser<'a> {
 }
 
 impl<'a> SecurityFrameParser<'a> {
-    pub fn new(buff: &'a [u8]) -> Self {
-        Self { buff }
+    pub fn uncheck_new(buff: &'a [u8]) -> Result<Self> {
+        Ok(Self {
+            buff: buff.get(1..).ok_or(Error::WrongLength(1))?,
+        })
     }
 
-    // pub fn frame_type(&self) -> Result<FrameType> {
-    //     let byte = self.buff.first().ok_or(Error::WrongLength(1))?;
-    //
-    //     Ok(FrameType::from_u8(*byte))
-    // }
-    //
-    // pub fn version(&self) -> Result<Version> {
-    //     let byte = self.buff.first().ok_or(Error::WrongLength(1))?;
-    //
-    //     Ok(Version::from_u8(*byte))
-    // }
-
     pub fn scheme(&self) -> Result<SecurityScheme> {
-        let byte = self.buff.get(1).ok_or(Error::WrongLength(2))?;
+        let byte = self.buff.first().ok_or(Error::WrongLength(2))?;
 
         Ok(SecurityScheme::from_u8(*byte))
     }
 
     pub fn dh_algorithm(&self) -> Result<DHAlgorithm> {
-        let byte = self.buff.get(1).ok_or(Error::WrongLength(2))?;
+        let byte = self.buff.first().ok_or(Error::WrongLength(2))?;
 
         Ok(DHAlgorithm::from_u8(*byte))
     }
 
     pub fn sponge_algorithm(&self) -> Result<SpongeAlgorithm> {
-        let byte = self.buff.get(1).ok_or(Error::WrongLength(2))?;
+        let byte = self.buff.first().ok_or(Error::WrongLength(2))?;
 
         Ok(SpongeAlgorithm::from_u8(*byte))
     }
 
     pub fn payload(&self) -> Result<&[u8]> {
-        self.buff.get(2..).ok_or(Error::WrongLength(3))
+        self.buff.get(1..).ok_or(Error::WrongLength(3))
     }
 }
